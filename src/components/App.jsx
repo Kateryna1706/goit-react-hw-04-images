2a35a0c196a65332cbdf940bc1b1f28f1994ac18
Kateryna1706 <silckatya@gmail.com>
2023-06-29 15:02:08 +0200

    added components and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({
+      contacts: parsedContacts,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div
+        style={{
+          // height: '100vh',
+          // display: 'flex',
+          // justifyContent: 'center',
+          // flexDirection: 'column',
+          // alignItems: 'flex-start',
+          // padding: 20,
+          // fontSize: 30,
+          // color: '#010101',
+
+          display: 'grid',
+          gridTemplateColumns: 1,
+          gridGap: 16,
+          paddingBottom: 24,
+        }}
+      ></div>
+    );
+  }
+}
